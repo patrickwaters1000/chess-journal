897f83aad8855d750c25cd8748dec6ca6d1e00a3
Patrick Waters <patrick.waters@liftoff.io>
2021-01-25 22:45:04 -0800

    Refactor endgame drills

--- front/endgameTrainer.js
+++ front/endgameTrainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Board from "./Board.js";
+import { drills } from "./endgameDrills.js";
+import { shuffle } from "lodash";
 
 const hflexStyle = {display: "flex", flexDirection: "row"}
 const vflexStyle = {display: "flex", flexDirection: "column"}
@@ -25,17 +27,42 @@ const getActiveColor = (fen) => {
   }
 };
 
+// Nice improvements I could make
+// 1 Handle checkmate / stalemate correctly on server side
+// 3 Show moves played
+// 4 Allow drills with Black to play
+
+//const shuffledDrills = shuffle(drills);
+const shuffledDrills = drills;
+
 var appState = {
-  // Winning king and pawn endgame
-  fen: "8/8/8/3k4/8/4K3/3P4/8 w - - 0 1",
-  color: "w", // color that user is playing as
+  commentBoxHidden: true,
+  drillIdx: 0,
+  fen: shuffledDrills[0].fen,
+  color: "w", // color that user is playing as; CURRENTLY NOT USED
   selectedPieceSquare: null,
-  flipBoard: false
+  flipBoard: false // CURRENTLY ALWAYS false
 };
 
-const logState = (msg) => {
-  console.log(msg, JSON.stringify(appState));
+const getDrillFEN = (idx) => {
+  return shuffledDrills[idx].fen;
 };
+
+const getDrillComment = (idx) => {
+  return shuffledDrills[idx].comment;
+};
+
+const resetDrill = () => {
+  appState.drillIdx %= shuffledDrills.length;
+  appState.fen = getDrillFEN(appState.drillIdx);
+  appState.selectedPieceSquare = null;
+  syncAppState();
+}
+
+const nextDrill = () => {
+  appState.drillIdx += 1;
+  resetDrill();
+}
 
 // Parses a FEN substring representing one rank of the board.
 // Example: "3P2q1" => ["","","","P","","","q",""]
@@ -93,10 +120,54 @@ const isPromotion = (fromSquare) => {
 const Button = (props) => {
   return React.createElement(
     "button",
-    { onClick: props.onClick },
+    {
+      onClick: props.onClick,
+      style: {
+	maxWidth: "150px"
+      }
+    },
     props.text
   );
 };
+
+const toggleCommentBox = () => {
+  appState.commentBoxHidden = !appState.commentBoxHidden;
+  syncAppState();
+};
+
+class CommentWidget extends React.Component {
+  render() {
+    let { drillIdx,
+	  commentBoxHidden } = this.props;
+    return React.createElement(
+      "div",
+      {
+	style: {
+	  ...vflexStyle,
+	  width: "400px",
+	  maxWidth: "400px",
+	  minHeight: "300px",
+	  height: "300px"
+	}
+      },
+      React.createElement(
+	// For some reason that I don't understand, the component
+	// won't update if it's a "textarea" instead of a paragraph.
+	"p",
+	null,
+	(commentBoxHidden
+	 ? ""
+	 : getDrillComment(drillIdx))
+      ),
+      Button({
+	text: (commentBoxHidden
+	       ? "Show comments"
+	       : "Hide comments"),
+	onClick: toggleCommentBox
+      })
+    );
+  };
+}
 
 class Page extends React.Component {
   constructor(props) {
@@ -119,6 +190,27 @@ class Page extends React.Component {
 	  clickPieceFn: clickPiece,
 	  clickSquareFn: clickSquare
 	}
+      ),
+      React.createElement(
+	"div",
+	{
+	  height: "75px",
+	  width: "200px",
+	  style: vflexStyle
+	},
+	Button({text: "Next drill",
+		onClick: nextDrill}),
+        Button({text: "Reset drill",
+		onClick: resetDrill}),
+	Button({text: "Get FEN",
+		onClick: () => { confirm(appState.fen); }}),
+	React.createElement(
+	  CommentWidget,
+	  {
+	    drillIdx: s.drillIdx,
+	    commentBoxHidden: s.commentBoxHidden
+	  }
+	)
       )
     );
   };
